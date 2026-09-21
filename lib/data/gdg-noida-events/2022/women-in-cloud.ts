8fa2816707c3f0975fba6597a22a6eb2e70ef473
Arsh <arshsrivastava00gmail.com>
2026-09-21 23:28:24 +0530

    year 2022

--- lib/data/gdg-noida-events/2022/women-in-cloud.ts
+++ lib/data/gdg-noida-events/2022/women-in-cloud.ts
@@ -30,9 +30,9 @@ export const womenInCloud: EventsCatalog["women-in-cloud"] = {
       "description": "This community-led event brings together developers, designers, and professionals to learn from each other, discover new ideas, and build meaningful connections in the local tech ecosystem."
     },
     "uniqueStats": {
-      "speakers": "7+",
-      "attendees": "153+",
-      "registered": "611+"
+      "speakers": "4+",
+      "attendees": "80+",
+      "registered": "320+"
     },
     "feedback": {
       "heading": "What attendees said",
@@ -48,56 +48,65 @@ export const womenInCloud: EventsCatalog["women-in-cloud"] = {
           "sessions": [
             {
               "id": 1,
-              "startTime": "11:00",
-              "endTime": "",
+              "startTime": "10:45",
+              "endTime": "11:00",
               "title": "Icebreaker",
               "description": "",
               "speakers": []
             },
             {
               "id": 2,
-              "startTime": "11:10",
-              "endTime": "",
+              "startTime": "11:00",
+              "endTime": "11:10",
               "title": "Opening",
               "description": "",
               "speakers": []
             },
             {
               "id": 3,
-              "startTime": "12:00",
-              "endTime": "",
-              "title": "Get to know about Cloud Shrankhla Srivastava --",
+              "startTime": "11:10",
+              "endTime": "12:00",
+              "title": "Get to know about Cloud",
               "description": "",
-              "speakers": []
+              "speakers": [
+                {
+                  "name":"Shrankla Srivastava"
+                }
+              ]
             },
             {
               "id": 4,
-              "startTime": "12:15",
-              "endTime": "",
-              "title": "Cloud Security 101 Pooja Gera Software Engineer, Palo Alto Networks",
+              "startTime": "12:00",
+              "endTime": "12:15",
+              "title": "Cloud Security 101",
               "description": "",
-              "speakers": []
+              "speakers": [
+                {
+                  "name":"Pooja Gera",
+                  "designation":"Software Engineer"
+                }
+              ]
             },
             {
               "id": 5,
-              "startTime": "12:50",
-              "endTime": "",
+              "startTime": "12:20",
+              "endTime": "12:50",
               "title": "Everything Technical (Breakout)",
               "description": "",
               "speakers": []
             },
             {
               "id": 6,
-              "startTime": "13:25",
-              "endTime": "",
+              "startTime": "12:55",
+              "endTime": "13:25",
               "title": "Lunch",
               "description": "",
               "speakers": []
             },
             {
               "id": 7,
-              "startTime": "14:00",
-              "endTime": "",
+              "startTime": "13:30",
+              "endTime": "14:00",
               "title": "Beyond the Binary (Breakout)",
               "description": "",
               "speakers": []
@@ -153,176 +162,7 @@ export const womenInCloud: EventsCatalog["women-in-cloud"] = {
           "label": "All"
         }
       ],
-      "images": [
-        {
-          "id": 1,
-          "src": "https://json.commudle.com/rails/active_storage/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaEtkIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--16884ac63b927f0d798c820b43212a7562c18b40/16.9%20womenin%20cloud.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 2,
-          "src": "https://json.commudle.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZ3UzIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--c793a76a579714d72bf7e51b0fe9d4d13640a460/commudle-logo-full.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 3,
-          "src": "https://json.commudle.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNmZLQVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--d66f9155a4bb4bdf4dfbc9a4cc77613dd7709893/Commudle%20Logo.svg",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 4,
-          "src": "https://json.commudle.com/icons/dark-mode-sun-icon.svg",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 5,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbzhkIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--ed0b3e638d73483e10a8858050ae95e07a925dad/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--2435c0338fa7cddd280234f2da44100890e88310/GDG%20Noida%20Light%20Horizontal-Logo.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 6,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBc3VmIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--49127d0dfcc9c141cbe6b77f59c010514151513c/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVNWcEpUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--3ddca6f6c247c2a968f56480e328f90094ff9d95/avatar.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 7,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBK3FCQVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--4f27d7fc7a8a9e78e6cc4f8301461a76f508fff4/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJYW5CbkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVNWcEpUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--cc9745bc98d3057a1d009e2fc5cce6fc9e479f67/IMG_2106.jpg",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 8,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcmVDIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2af49c60614553ff586164bc342f23080ff6894e/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lKYW5CbFp3WTZCa1ZVT2hSeVpYTnBlbVZmZEc5ZmJHbHRhWFJiQjJrbGFTVTZDMnh2WVdSbGNuc0dPZ2x3WVdkbE1BPT0iLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--29ce9b44dfab0ad85674a84329340b5260fa5c3f/google_28663.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 9,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBdUVkIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--99778e5cd656337c83c763642c0c036fb07171b6/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVNWcEpUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--3ddca6f6c247c2a968f56480e328f90094ff9d95/Deekshita_Verma.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 10,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBc3VmIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--49127d0dfcc9c141cbe6b77f59c010514151513c/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--2435c0338fa7cddd280234f2da44100890e88310/avatar.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 11,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBdUVkIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--99778e5cd656337c83c763642c0c036fb07171b6/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--2435c0338fa7cddd280234f2da44100890e88310/Deekshita_Verma.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 12,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBK3FCQVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--4f27d7fc7a8a9e78e6cc4f8301461a76f508fff4/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJYW5CbkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--d98d8d7f00147e4586d1b19b8f18ed1c04902eb8/IMG_2106.jpg",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 13,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcmVDIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2af49c60614553ff586164bc342f23080ff6894e/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lKYW5CbFp3WTZCa1ZVT2hSeVpYTnBlbVZmZEc5ZmJHbHRhWFJiQjJsRmFVVTZDMnh2WVdSbGNuc0dPZ2x3WVdkbE1BPT0iLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--1f38ba38cbd08e0e52ad1086f1646943350cad48/google_28663.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 14,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBOXVLQXc9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--f0f13efd2b7eff4b5129ee1f5a62cdc01411001e/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJYW5CbkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--d98d8d7f00147e4586d1b19b8f18ed1c04902eb8/WhatsApp%20Image%202024-11-13%20at%2020.57.52_47245872.jpg",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 15,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbytmIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--0460c1405a1d8d0c1e0a03778fd4bc8647fd5b39/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--2435c0338fa7cddd280234f2da44100890e88310/avatar.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 16,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbzJmIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--45576da602681caf5fe0053d3ee1252f02f5fee5/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--2435c0338fa7cddd280234f2da44100890e88310/avatar.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 17,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcW9mIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--31f0655160488d3debb1590da5239ae4a961035e/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lKYW5CbFp3WTZCa1ZVT2hSeVpYTnBlbVZmZEc5ZmJHbHRhWFJiQjJsRmFVVTZDMnh2WVdSbGNuc0dPZ2x3WVdkbE1BPT0iLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--1f38ba38cbd08e0e52ad1086f1646943350cad48/Tanya%20LinkedIn%20dp.jpeg",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 18,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbGlXIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--5ee49420535bcbcf78f9613e76fa28f36784c7f3/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lKYW5CbFp3WTZCa1ZVT2hSeVpYTnBlbVZmZEc5ZmJHbHRhWFJiQjJsRmFVVTZDMnh2WVdSbGNuc0dPZ2x3WVdkbE1BPT0iLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--1f38ba38cbd08e0e52ad1086f1646943350cad48/avatar.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 19,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbDZkIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--5c8453be02537d9e303e8b32ade979d8c955e234/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--2435c0338fa7cddd280234f2da44100890e88310/avatar.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 20,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcEdkIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--e3d0b8976e54c99e6d46691de6019e98b0c2d11e/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--2435c0338fa7cddd280234f2da44100890e88310/avatar.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 21,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcU9kIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--f8d9f0e4d8b6aca89e4b8cf58bbdb8fa340463b2/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJYW5CbkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--d98d8d7f00147e4586d1b19b8f18ed1c04902eb8/IMG_20220521_224047.jpg",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 22,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBdjJkIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--fea6d71f54da3d4378d8d5115673f67d15ec3351/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lKYW5CbFp3WTZCa1ZVT2hSeVpYTnBlbVZmZEc5ZmJHbHRhWFJiQjJsRmFVVTZDMnh2WVdSbGNuc0dPZ2x3WVdkbE1BPT0iLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--1f38ba38cbd08e0e52ad1086f1646943350cad48/1659890004770-01~2.jpeg",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 23,
-          "src": "https://json.commudle.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaHVmIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--0006911868fe9603ab62d8b35fc82f40daa9c3cd/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RkhKbGMybDZaVjkwYjE5c2FXMXBkRnNIYVVWcFJUb0xiRzloWkdWeWV3WTZDWEJoWjJVdyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--2435c0338fa7cddd280234f2da44100890e88310/avatar.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        },
-        {
-          "id": 24,
-          "src": "https://json.commudle.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZzYzIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--ec4a1cbde1227bc84c1fc145325263b3dfb219e4/commudle-logo128.png",
-          "alt": "Womxn In Cloud",
-          "category": "all",
-          "aspectRatio": 1
-        }
-      ]
+      "images": []
     }
   }
 };
